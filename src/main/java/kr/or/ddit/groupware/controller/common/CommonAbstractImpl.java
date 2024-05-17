671b7ac4e05e3fabad6164c8915d15fbc65e1df3
305SEM-PC <305SEM-PC@DESKTOP-7772JQU>
2024-05-17 14:17:54 +0900

    2024.05.17_ThinkLink_Commit

---
+++ src/main/java/kr/or/ddit/groupware/controller/common/CommonAbstractImpl.java
@@ -0,0 +1,18 @@
+package kr.or.ddit.groupware.controller.common;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.or.ddit.groupware.vo.CustomUser;
+import kr.or.ddit.groupware.vo.EmployeeVO;
+
+public abstract class CommonAbstractImpl {
+	
+	protected ObjectMapper mapper = new ObjectMapper();
+	
+	public EmployeeVO getEmployee() {
+		return ((CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmployeeVO();
+	}
+	
+}
